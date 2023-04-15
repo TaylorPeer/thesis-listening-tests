f6306d84463407eb67ee19e93b28e3a83ff54b92
TaylorPeer <t.peer@cortical.io>
2023-04-15 23:54:54 +0200

    Trying to remove streamlit branding

--- listening-test.py
+++ listening-test.py
@@ -25,7 +25,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            div[class^='viewerBadge_link'] {visibility: hidden;}
+            div[class^='viewerBadge_link'] {visibility: hidden !important;}
             </style>
             """
 content_width_style = """
